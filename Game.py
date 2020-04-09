686883c0496f1e7b798355df0985cac6e4b29067
Cherevko88 <61087574+Cherevko88@users.noreply.github.com>
2020-04-10 00:13:35 +0300

    Update Game.py

--- Game.py
+++ Game.py
@@ -4,6 +4,7 @@ class Unit(ABC):
     _name = None
     _strength = 0
     _health = 0
+    _doubleattack = 0
 
     def _cheak_name(self, name):
         return name
@@ -12,6 +13,11 @@ class Unit(ABC):
         self._name = self._cheak_name(name)
         self._strength = strength
         self.health = health
+
+    def _cheak_double_attack(self):
+        crit = random.random()
+        if self._doubleattack >= crit:
+            return True
 
     @abstractmethod
     def _attack(self, opponent):
@@ -23,6 +29,7 @@ class Unit(ABC):
         if opponent._health == 0:
             raise Exception(f'Здоровье противника = "{opponent.health}"')
 
+
         if isinstance(opponent, Rogue):
             r = random.randint(1, 10)
             if r == 2:
@@ -32,9 +39,11 @@ class Unit(ABC):
         else:
             return self._attack(opponent)
 
+
     @property
     def health(self):
         return self._health
+
 
     @health.setter
     def health(self, val):
@@ -43,50 +52,60 @@ class Unit(ABC):
 
         self._health = val if val > 0 else 0
 
-class Vampire(Unit):
 
+class Vampire(Unit):
+    _doubleattack = 0.5
     def _attack(self, opponent):
         dmg = self._strength
+        if self._cheak_double_attack():
+            dmg = dmg * 2
         opponent.health -= dmg
         self.health += dmg * 0.1
 
+
 class Knight(Unit):
-
+    _doubleattack = 0.4
     def _attack(self, opponent):
-
         dmg = self._strength
-        opponent.health -= dmg
+        if self._cheak_double_attack():
+            dmg = dmg * 2
+        opponent.health -= dmg * 1.2
+
 
 class Monk(Unit):
-
+    _doubleattack = 0.7
     def _attack(self, opponent):
-        dmg = opponent._strength
+        dmg = self._strength
+        if self._cheak_double_attack():
+            dmg = dmg * 2
         opponent.health -= dmg
         self._health = round(self._health * 1.1)
 
-class Rogue(Unit):
 
+class Rogue(Unit):
+    _doubleattack = 0.4
     def _attack(self, opponent):
         dmg = self._strength
+        if self._cheak_double_attack():
+            dmg = dmg * 2
         opponent.health -= dmg
 
-k1 = Knight(name="Artur", strength=100, health=100000)
-v1 = Vampire(name="Drac", strength=10, health=1000)
-r1 = Rogue(name="Krok", strength=150, health=2000)
-m1 = Monk(name="Panda Kunfu", strength=90, health=1000)
 
-print(m1.health)
+k1 = Knight(name="Artur", strength=100, health=500)
+v1 = Vampire(name="Drac", strength=100, health=500)
+r1 = Rogue(name="Krok", strength=150, health=500)
+m1 = Monk(name="Panda Kunfu", strength=90, health=500)
 
-m1.attack(k1)
-print(m1.health)
-m1.attack(k1)
-print(m1.health)
-m1.attack(k1)
-print(m1.health)
-m1.attack(k1)
-print(m1.health)
-m1.attack(k1)
-print(m1.health)
-m1.attack(k1)
-print(m1.health)
-
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
+k1.attack(r1)
+print(r1._health)
